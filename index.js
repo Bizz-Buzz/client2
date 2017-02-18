c316495ee802086430b4c2ce53f8c9f17f449b2b
harrison-symes <symeshjb@gmail.com>
2017-02-19 02:54:55 +1300

    Updates

---
+++ index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/app'
+import Router from 'sheet-router'
+import { createStore } from 'redux'
+
+import Login from './components/login'
+import Signup from './components/signup'
+import Main from './components/main'
+
+var reducer = require('./reducer')
+
+var app = document.createElement('div')
+document.querySelector('main').appendChild(app)
+
+const initialState = {
+  theme: 'default',
+  title: "Bizz-Buzz",
+  route: '/',
+  view: 'events',
+  isLoading: false,
+  loginDetails: {
+    username: ' ',
+    password: ' '
+    },
+  signupDetails: {},
+  authError: null,
+  user: {},
+  bizz: null,
+  events: [],
+  createEventToggle: false,
+  posts: [],
+  createPostToggle: false,
+  createPost: '',
+  selectedPost: null,
+  postRespones: []
+
+}
+
+var store = createStore(reducer, initialState)
+const {getState, dispatch, subscribe} = store
+
+
+const route = Router({ default: '/404' }, [
+  ['/', (params) => Login],
+  ['/signUp', (params) => Signup],
+  ['/bizz', (params) => Main]
+])
+
+subscribe(() => {
+  console.log(getState().route);
+  const Component = route(getState().route)
+  render(<Component state={getState()} dispatch={dispatch} />, app)
+})
+
+render(<App name='bizzBuzz' />, app)
+console.log('welcome to bizzBuzz')
+
+dispatch({type: 'INIT'})
---
+++ reducer.js
@@ -0,0 +1,67 @@
+const clone = require('clone')
+
+module.exports = (state, action) => {
+  var newState = clone(state)
+  const {payload, type} = action
+  switch (type) {
+    case "TOGGLE_LOADING":
+      console.log("loading toggled");
+      newState.isLoading = !newState.isLoading
+      return newState
+    case "AUTH_ERROR":
+      newState.authError = payload
+      return newState
+    case 'UPDATE_LOGIN_DETAILS':
+      newState.loginDetails[payload.change] = payload.value
+      return newState
+    case 'UPDATE_SIGNUP_DETAILS':
+      newState.signupDetails[payload.change] = payload.value
+      console.log(newState.signupDetails);
+      return newState
+    case 'LOGIN_SUCCESS':
+      newState.user = payload.user
+      newState.events = payload.events
+      newState.route = '/bizz'
+      newState.view = 'events'
+      return newState
+    case 'SIGNUP_SUCCESS':
+      newState.route = '/login'
+      newState.authError = "Signup Successful, please log in"
+      return newState
+    case 'CHANGE_ROUTE':
+      newState.route = payload
+      newState.authError = null
+      return newState
+    case 'CHANGE_VIEW':
+      newState.view = payload
+      return newState
+    case 'RECIEVE_CONTENT':
+      newState[payload.content_type] = payload.content
+      newState.view = payload.content_type
+      return newState
+    case 'UPDATE_CREATE_POST':
+      newState.createPost = payload
+      return newState
+    case 'POST_POST':
+      newState.posts.unshift({
+        post_id: payload,
+        content: newState.createPost,
+        first_name: newState.user.first_name,
+        last_name: newState.user.last_name,
+        post_created_at: 'Just Now',
+        responses: 0
+      })
+      newState.createPost = ''
+      newState.createPostToggle = false
+      return newState
+    case 'GET_POST_RESPONES':
+      newState.selectedPost = payload.post_id
+      newState.postResponses = payload.responses
+      return newState
+    case 'TOGGLE':
+      newState[payload] = !newState[payload]
+      return newState
+    default:
+      return newState
+  }
+}
