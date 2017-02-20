51adc9ada68bac654f0dfc30b4cf888b4cc8a4de
mischa-saunders <mischa.saunders@gmail.com>
2017-02-20 20:18:48 +1300

    began rendering groups you have joined, toggle for creating group (form incomplete), button for finding groups inactive

---
+++ components/views/groups.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import renderCreateGroup from './createGroup'
+
+module.exports = (state, dispatch) => {
+  function renderGroup (group) {
+    return <div className='group'>
+			<p className="groupName">{group.group_name}</p>
+			<p className="groupMemberCount">{group.member_count} members</p>
+    </div>
+  }
+  function renderGroups () {
+    return state.groups.map((group) => renderGroup(group))
+  }
+  return <div className="groups">
+			<button onClick={() => requestUnjoinedGroups(state, dispatch)}>Find Groups</button>
+
+			{state.createGroupToggle
+	      ? renderCreateGroup(state, dispatch)
+	      : <button className="createGroupButton" onClick={() => dispatch({type: 'TOGGLE', payload: 'createGroupToggle'})}>New Group</button>
+	    }
+      {renderGroups()}
+  </div>
+}
