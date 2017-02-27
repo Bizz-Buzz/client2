3157c6cc338abe29327c5c45636e2f4e7079a111
harrison-symes <symeshjb@gmail.com>
2017-02-27 17:22:59 +1300

    added select dropdown for group to post to in create post

--- components/views/createPost.js
+++ components/views/createPost.js
@@ -7,6 +7,22 @@ module.exports = (state, dispatch) => {
       content, content_type
     }})
   }
+  function renderOption(option) {
+    if (option.group_id != state.currentGroup.group_id) {
+      return <option value={option.group_id}>{option.group_name}</option>
+    } else return <p></p>
+
+	}
+  function groupHandler() {
+		var parentOptions = state.groups.filter((group) => {
+			return group.isAdmin
+		})
+		return <select className="selectParentGroup" onChange={(e) => updatePostDetails(e.target.value, 'parent_id')} >
+			<p className="selectParentGroup" >Post to</p>
+      <option value={state.currentGroup.group_id}>{state.currentGroup.group_name}</option>
+			{parentOptions.map((option) => renderOption(option))}
+		</select>
+	}
   function renderAlertOption() {
     if (state.createPost.is_alert) {
       return <p>Alert</p>
@@ -22,6 +38,7 @@ module.exports = (state, dispatch) => {
     <input type="text" className="detsInput postInput" onChange={(e) => updatePostDetails(e.target.value, 'content')}placeholder="content"/>
     <button className="createPostButtons leftButton" onClick={() => dispatch({type: 'TOGGLE', payload: 'createPostToggle'})}>Cancel</button>
     {alertHandler()}
+    {groupHandler()}
     {state.createPost.content.length > 10
     ? <button className="createPostButtons rightButton" onClick={() => postPost(state, dispatch)}>Post</button>
     : <p>Post too short</p>}
