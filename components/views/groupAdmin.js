01a68b0032e2550ea35b9a15821cc6b949007dc3
eda013 <poukupenga@gmail.com>
2017-03-09 03:58:08 +1300

    added blank page for adminView emergency

--- components/views/groupAdmin.js
+++ components/views/groupAdmin.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import renderAdminMessages from './adminMessages.js'
 import renderAdminLeaveRequests from './adminLeaveRequests'
+import renderAdminEmergency from './adminEmergency'
 
 module.exports = (state, dispatch) => {
   function renderAdminHeader() {
     return <div className="adminHeader">
       <p onClick={() => dispatch({type: 'CHANGE_ADMIN_VIEW', payload: 'adminMessages'})}>Messages ({state.admin.adminMessages.length})</p>
       <p onClick={() => dispatch({type: 'CHANGE_ADMIN_VIEW', payload: 'leaveRequests'})}>Leave ({state.admin.leaveRequests.length})</p>
-      <p>Emergency ({state.admin.emergency.length})</p>
+      <p onClick={() => dispatch({type: 'CHANGE_ADMIN_VIEW', payload: 'emergency'})}>Emergency ({state.admin.emergency.length})</p>
     </div>
   }
   function renderAdminView() {
@@ -16,6 +17,8 @@ module.exports = (state, dispatch) => {
         return renderAdminLeaveRequests(state, dispatch)
       case 'adminMessages':
         return renderAdminMessages(state, dispatch)
+      case 'emergency':
+        return renderAdminEmergency(state, dispatch)
       default:
         return renderAdminMessages(state, dispatch)
     }
