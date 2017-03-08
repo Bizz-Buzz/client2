64db19289eaba438965443d569806e0a9f9f6988
eda013 <poukupenga@gmail.com>
2017-03-09 01:46:17 +1300

    fixed the issues

--- components/views/emergencyReport.js
+++ components/views/emergencyReport.js
@@ -2,14 +2,10 @@ import React from 'react'
 
 
 module.exports = (state, dispatch) => {
-<<<<<<< HEAD
   return <div className="groups">
-    <button className="toggleButton" onClick={() => dispatch({type: 'CHANGE_VIEW', payload: 'admin'})}>Back</button>
-=======
-  return <div className="emergencyDiv">
->>>>>>> 52e93bbfdc7027fbae463364ef0d0d649bdbfb8f
     <div>
       Emergency Reporting
     </div>
+    <button className="toggleButton" onClick={() => dispatch({type: 'CHANGE_VIEW', payload: 'communication'})}>Back</button>
   </div>
 }
