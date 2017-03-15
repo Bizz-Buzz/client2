e20ffcf4b3de7f98e95f83db260ccaf909ccb126
eda013 <poukupenga@gmail.com>
2017-03-15 22:35:01 +1300

    not working yet

--- components/views/admin.js
+++ components/views/admin.js
@@ -2,19 +2,19 @@ import React from 'react'
 import requestAdminData from '../../services/requestAdminData'
 
 module.exports = (state, dispatch) => {
-  // function renderGroupAdmin() {
-  //   var groups = state.groups.filter((group) => {
-  //     return group.isAdmin
-  //   })
-  //   console.log({groups});
-  //   if (groups.length != 0) {
-  //     return <button className="messageAdminsButton" onClick={() => requestAdminData(state, dispatch)}>Group Admin</button>
-  //   }
-  // }
-  // return <div className="communication">
-  //   <button className="leaveRequestButton" onClick={() => dispatch({type: 'CHANGE_VIEW', payload: 'requestLeave'})}>Request Leave</button>
-  //   <button className="emergencyButton" onClick={() => dispatch({type: 'CHANGE_VIEW', payload: 'emergency'})}>Emergency</button>
-  //   <button className="messageAdminsButton" onClick={() => dispatch({type: 'CHANGE_VIEW', payload: 'messageAdmin'})}>Message Admins</button>
-  //   {renderGroupAdmin()}
-  // </div>
+  function renderGroupAdmin() {
+    var groups = state.groups.filter((group) => {
+      return group.isAdmin
+    })
+    console.log({groups});
+    if (groups.length != 0) {
+      return <button className="messageAdminsButton" onClick={() => requestAdminData(state, dispatch)}>Group Admin</button>
+    }
+  }
+  return <div className="communication">
+    <button className="leaveRequestButton" onClick={() => dispatch({type: 'CHANGE_VIEW', payload: 'requestLeave'})}>Request Leave</button>
+    <button className="emergencyButton" onClick={() => dispatch({type: 'CHANGE_VIEW', payload: 'emergency'})}>Emergency</button>
+    <button className="messageAdminsButton" onClick={() => dispatch({type: 'CHANGE_VIEW', payload: 'messageAdmin'})}>Message Admins</button>
+    {renderGroupAdmin()}
+  </div>
 }
