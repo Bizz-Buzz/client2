0a31c2b503f10927c1f0c7a9d475166c21cc78e5
JasmineGWT <jazzgwwt@gmail.com>
2017-03-15 18:50:05 +1300

    Group admin lookin sexi

--- components/views/adminMessages.js
+++ components/views/adminMessages.js
@@ -4,8 +4,8 @@ module.exports = (state, dispatch) => {
   function renderAdminMessage(message) {
     return <div className="adminMessage">
       <div className="adminMessageFrom">{message.first_name} {message.last_name}</div>
-      <div className="adminMessageGroup">{message.group_name}</div>
       <div className="adminMessageContent">{message.content}</div>
+      <div className="adminMessageGroup">{message.group_name}</div>
       <div className="adminMessageCreated">{message.message_created_at}</div>
     </div>
   }
